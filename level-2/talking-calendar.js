d717fda38ec6fff7e29b162c2402f839066773be
Nic <nic.ehmayer92@gmail.com>
2018-01-04 07:14:07 +0000

    Replaced if statements with switch to find the month name

--- level-2/talking-calendar.js
+++ level-2/talking-calendar.js
@@ -4,31 +4,73 @@ var talkingCalendar = function(date) {
   var month = date.slice(5, 7);
   var day = date.slice(8, 10);
 
-  if (month === '01') {
+  // Replaces if statements below.
+  switch (month) {
+    case '01':
+      month = 'January';
+      break;
+    case '02':
+      month = 'February';
+      break;
+    case '03':
+      month = 'March';
+      break;
+    case '04':
+      month = 'April';
+      break;
+    case '05':
+      month = 'May';
+      break;
+    case '06':
+      month = 'June';
+      break;
+    case '07':
+      month = 'July';
+      break;
+    case '08':
+      month = 'Augest';
+      break;
+    case '09':
+      month = 'September';
+      break;
+    case '10':
+      month = 'October';
+      break;
+    case '11':
+      month = 'November';
+      break;
+    case '12':
+      month = 'December';
+      break;
+  }
+
+/*
+  if (month == 01) {
     month = "January";
-  } else if (month === '02') {
+  } else if (month == 03) {
     month = "February";
-  } else if (month === '03') {
+  } else if (month == 03) {
     month = "March";
-  } else if (month === '04') {
+  } else if (month == 04) {
     month = "April";
-  } else if (month === '05') {
+  } else if (month == 05) {
     month = "May";
-  } else if (month === '06') {
+  } else if (month == 06) {
     month = "June";
-  } else if (month === '07') {
+  } else if (month == 07) {
     month = "July";
-  } else if (month === '08') {
+  } else if (month == 08) {
     month = "Augest";
-  } else if (month === '09') {
+  } else if (month == 09) {
     month = "September";
-  } else if (month === '10') {
+  } else if (month == 10) {
     month = "October";
-  } else if (month === '11') {
+  } else if (month == 11) {
     month = "November";
-  } else if (month === '12') {
+  } else if (month == 12) {
     month = "December";
   }
+*/
 
   var dayHundredth = day % 10;
 
@@ -54,5 +96,5 @@ console.log(talkingCalendar('2017/12/02'));
 console.log(talkingCalendar('2007/11/11'));
 console.log(talkingCalendar('1987/08/24'));
 
-console.log(talkingCalendar('1992/01/13'));
+console.log(talkingCalendar('1992/01/11'));
 
