23a852434c82e708fd70c23ca64ae078c5a44b3b
Nic <nic.ehmayer92@gmail.com>
2018-01-07 07:25:34 +0000

    Added first two level-2 challenges

---
+++ level-2/camel-case-maker.js
@@ -0,0 +1,20 @@
+var camelCase = function(input) {
+  var words = input.split(" ");
+  var output = [];
+
+  words[0] = words[0].toLowerCase();
+  output.push(words[0]);
+
+  for (var i = 1; i < words.length; i++) {
+    var firstLetter = words[i][0].toUpperCase();
+    output.push(words[i]);
+    output[i] = output[i].toLowerCase().replace(output[i][0], firstLetter);
+    //output[i] = output[i].replace(output[i][0], firstLetter);
+  }
+  return output.join('');
+  //return words;
+}
+
+console.log(camelCase('THIS is a stRIng'));
+console.log(camelCase('loopy lighthouse'));
+console.log(camelCase('supercalifragalisticexpialidocious'));
---
+++ level-2/change-calculator.js
@@ -0,0 +1,64 @@
+var calculateChange = function(total, cash) {
+
+  var r = Math.round(100 * (cash - total))/100;
+
+  var moneyType = {
+    twentyDollar: 0,
+    tenDollar: 0,
+    fiveDollar: 0,
+    twoDollar: 0,
+    oneDollar: 0,
+    quarter: 0,
+    dime: 0,
+    nickel: 0,
+    penny: 0
+  };
+
+  while (r > 0) {
+    if (r - 20 >= 0) {
+      moneyType.twentyDollar ++;
+      r -= 20;
+    } else if (r - 10 >= 0) {
+      moneyType.tenDollar ++;
+      r -= 10;
+    } else if (r - 5 >= 0) {
+      moneyType.fiveDollar ++;
+      r -= 5;
+    } else if (r - 2 >= 0) {
+      moneyType.twoDollar ++;
+      r -= 2;
+    } else if (r - 1 >= 0) {
+      moneyType.oneDollar ++;
+      r -= 1;
+    } else if (r - 0.25 >= 0) {
+      moneyType.quarter ++;
+      r -= 0.25;
+    } else if (r - 0.10 >= 0) {
+      moneyType.dime ++;
+      r -= 0.10;
+    } else if (r - 0.05 >= 0) {
+      moneyType.nickel ++;
+      r -= 0.05;
+    } else if (r - 0.01 >= 0) {
+      moneyType.penny ++;
+      r -= 0.01;
+    } else {
+      break;
+    }
+  }
+  /*
+  for (i in moneyType) {
+    if ()
+  }
+  */
+  return moneyType;
+}
+
+
+
+console.log(calculateChange(17.87, 20));
+console.log(calculateChange(26.23, 40));
+console.log(calculateChange(5.01, 10));
+
+//console.log(calculateChange(10, 100));
+
